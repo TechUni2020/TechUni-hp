757d277e8666a3edab738223a7227f15b9fa2ca1
井手翔陽 <shouhi605@gmail.com>
2022-04-28 18:20:09 +0900

    Homeページのレスポンシブ修正

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -23,6 +23,7 @@ function Home() {
               </h1>
               <h1 className="heading-name">
                 プログラミング研究会
+                <br></br>
                 <strong className="main-name"> Tech.Uni</strong>
               </h1>
               <div style={{ padding: 50, textAlign: "center" }}>
