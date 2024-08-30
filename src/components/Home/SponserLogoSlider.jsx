6cd8fa2fbfb5e4cee9d4cde6b3180e200568cbae
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2024-08-31 07:03:56 +0900

    change: スクロールをcssで制御するように変更

--- src/components/Home/SponserLogoSlider.jsx
+++ src/components/Home/SponserLogoSlider.jsx
@@ -8,83 +8,23 @@ import Kobe from "../../assets/Sponsor/kobe.png";
 import Paiza from "../../assets/Sponsor/paiza.png";
 import Web3Career from "../../assets/Sponsor/web3-career.png";
 import SponsorLogo from "../Sponsor/SponsorLogo";
-import SlideContainer, { SliderItem } from "./slider";
+import SlideContainer from "./slider";
 
 function SponsorLogoSlider() {
-    return(
-        <>
-    <SlideContainer contentWidth={1600} initialOffsetX={0}>
-      <SliderItem width={150}>
-        <SponsorLogo logo={TechFul} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={TechTrain} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={TrackJob} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={HackBar} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Karabiner} maxHeight="400px"/>
-      </SliderItem>
-            <SliderItem width={150}>
-        <SponsorLogo logo={TechFul} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={TechTrain} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={TrackJob} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={HackBar} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Karabiner} maxHeight="400px"/>
-      </SliderItem>
-    </SlideContainer>
-    <SlideContainer contentWidth={1600} initialOffsetX={285}>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Kobe} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Paiza} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Web3Career} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Kobe} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Paiza} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Web3Career} maxHeight="400px"/>
-      </SliderItem>
-            <SliderItem width={150}>
-        <SponsorLogo logo={Kobe} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Paiza} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Web3Career} maxHeight="400px"/>
-      </SliderItem>
-            <SliderItem width={150}>
-        <SponsorLogo logo={Kobe} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Paiza} maxHeight="400px"/>
-      </SliderItem>
-      <SliderItem width={150}>
-        <SponsorLogo logo={Web3Career} maxHeight="400px"/>
-      </SliderItem>
-    </SlideContainer>
-  </>
-);
+  const topLogos = [TechFul, TechTrain, TrackJob, HackBar];
+  const bottomLogos = [Karabiner, Kobe, Paiza, Web3Career];
+  return (
+    <>
+      <SlideContainer
+        top={[...topLogos, ...topLogos].map((logo, index) => (
+          <SponsorLogo key={index} logo={logo} />
+        ))}
+        bottom={[...bottomLogos, ...bottomLogos, ...bottomLogos].map((logo, index) => (
+          <SponsorLogo key={index} logo={logo} />
+        ))}
+      />
+    </>
+  );
 }
 
 export default SponsorLogoSlider;
