c26b113598d6d461563195ab1822cf4f2694d7ec
KO1231 <k.oka@kazutech.jp>
2025-01-02 19:28:38 +0900

    update: Sponsor logo in home

--- src/components/Sponsor/SponsorLogo.jsx
+++ src/components/Sponsor/SponsorLogo.jsx
@@ -6,7 +6,7 @@ function SponsorLogo(props) {
             <source srcSet={props.logo} type="image/webp"/>
             <img
                 src={props.logoAlt}
-                alt={"Sponsor's logo(" + props.name + ")"}
+                alt={"Sponsor's logo (" + props.name + ")"}
                 className="img-fluid"
                 style={{maxHeight: props.maxHeight, marginBottom: props.marginBottom}}
             />
