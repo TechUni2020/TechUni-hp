3a5cbbc85297071614fa8c27dbc2376d1b72108a
KO1231 <k.oka@kazutech.jp>
2025-01-19 16:59:01 +0900

    change: footerをdiv->footerタグに変更

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   let date = new Date();
   let year = date.getFullYear();
   return (
-          <Container fluid className="footer">
+          <Container as="footer" fluid className="footer">
             <Row>
               <Col md="4" className="footer-copywright">
                 <h3>IT系学生団体 Tech.Uni</h3>
