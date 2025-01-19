7347ae59bd5eccffb7f637e3eb9ecb20ba740561
Kazuhiro Oka <k.oka@kazutech.jp>
2025-01-19 18:26:56 +0900

    add: notfoundページを追加 (#111)

--- src/index.jsx
+++ src/index.jsx
@@ -12,6 +12,7 @@ import History2022 from "./components/History/years/History2022";
 import History2023 from "./components/History/years/History2023";
 import History2024 from "./components/History/years/History2024";
 import Sponsor from "./components/Sponsor/Sponsor";
+import NotFound from "./components/NotFound/NotFound";
 
 ReactDOM.render(
         <React.StrictMode>
@@ -21,20 +22,19 @@ ReactDOM.render(
                 <Route index element={<Home/>}/>
                 <Route path="project" element={<Projects/>}/>
                 <Route path="about" element={<About/>}/>
-                <Route path="history" element={<Home/>}/>
+                <Route path="history" element={<NotFound/>}/> {/* ~/Historyは配置していない。 */}
                 <Route path="history/*" element={(
-                        <section>
-                          <Outlet/>
-                        </section>
+                        <Outlet/>
                 )}>
                   <Route path="2020" element={<History2020/>}/>
                   <Route path="2021" element={<History2021/>}/>
                   <Route path="2022" element={<History2022/>}/>
                   <Route path="2023" element={<History2023/>}/>
                   <Route path="2024" element={<History2024/>}/>
+                  <Route path="*" element={<NotFound/>}/>
                 </Route>
                 <Route path="sponsor" element={<Sponsor/>}/>
-                <Route path="*" element={<Home/>}/>
+                <Route path="*" element={<NotFound/>}/>
               </Route>
             </Routes>
           </BrowserRouter>
